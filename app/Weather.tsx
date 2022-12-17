44845c1fbaab7349b60d28562d5e950e48e29202
Johnny Kang <junyoung@my.yorku.ca>
2022-12-17 02:19:48 -0500

    Weather

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,8 +1,100 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { getFormattedWeatherData } from "../lib/fetchWeather";
+
+type WeatherData = {
+  description: any;
+  temp: any;
+  iconURL: any;
+  feels_like: any;
+  temp_min: any;
+  temp_max: any;
+  pressure: any;
+  humidity: any;
+  speed: any;
+  country: any;
+  name: any;
+};
 
 function Weather() {
+  const [celcius, setCelcius] = useState(false);
+  const [weather, setWeather] = useState<WeatherData>();
+
+  useEffect(() => {
+    // const fetchWeather = async () => {
+
+    const fetchWeatherData = async () => {
+      navigator.geolocation.getCurrentPosition(async function (position) {
+        const data: WeatherData = await getFormattedWeatherData(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+        setWeather(data);
+      });
+    };
+
+    fetchWeatherData();
+  }, []);
+
+  const handleUnitsClick = () => {
+    setCelcius(!celcius);
+  };
+
+  let weatherDescription = weather?.description
+    .split(" ")
+    .map((word: string) => {
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    .join(" ");
+
   return (
-    <div className="grid grid-rows-3 font-sans text-xs md:text-base"></div>
+    <div className="font-sans text-base dark:text-[#3A160E] md:text-base">
+      {weather && (
+        <div className="h-30 flex flex-col md:flex-row">
+          <div className="h-full align-middle">
+            <img src={weather.iconURL} alt="weatherIcon" />
+            <div className="hidden sm:block md:text-xl">
+              {weatherDescription ?? weatherDescription}
+            </div>
+          </div>
+          <div className="my-auto align-middle md:text-3xl">
+            <div className="md:mt-6 md:ml-2">
+              <div className="underline">{`${weather.name}, ${weather.country}`}</div>
+              <div onClick={handleUnitsClick}>
+                {`${
+                  celcius === true
+                    ? (weather.temp - 273.15).toFixed(2) + "°C"
+                    : (((weather.temp - 273.15) * 9) / 5 + 32).toFixed(2) + "°F"
+                }
+                `}
+              </div>
+              <div className="flex flex-row md:text-base">
+                <div onClick={handleUnitsClick}>
+                  {`${
+                    celcius === true
+                      ? (weather.temp_max - 273.15).toFixed(2) + "°C"
+                      : (((weather.temp_max - 273.15) * 9) / 5 + 32).toFixed(
+                          2
+                        ) + "°F"
+                  }
+                `}
+                </div>
+                <div onClick={handleUnitsClick}>
+                  {`${
+                    celcius === true
+                      ? (weather.temp_min - 273.15).toFixed(2) + "°C"
+                      : (((weather.temp_min - 273.15) * 9) / 5 + 32).toFixed(
+                          2
+                        ) + "°F"
+                  }
+                `}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ lib/fetchWeather.ts
@@ -0,0 +1,38 @@
+const makeIconURL = (icon: string) =>
+  `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+const getFormattedWeatherData = async (lat: Number, lon: Number) => {
+  //   const URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=20&appid=${process.env.WEATHER_API_KEY}`;
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&limit=20&appid=713f964a0a59c33b670434c772385769`;
+
+  const weatherData = await fetch(weatherURL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+  console.log(weatherData);
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = weatherData;
+
+  const { description, icon } = weather[0];
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+
+export { getFormattedWeatherData };
