0eca57cf586ddbc6329603dd615c54155b734a79
Johnny Kang <junyoung@my.yorku.ca>
2023-01-10 01:04:38 -0500

    Time

--- app/CurrentTime.tsx
+++ app/CurrentTime.tsx
@@ -2,18 +2,105 @@
 
 import { useState, useEffect } from "react";
 
-function CurrentTime({ initialTime }: any) {
-  const [currentTime, setCurrentTime] = useState(initialTime);
+var today = new Date();
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var mL = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var yyyy = today.getFullYear();
+
+export function CurrentVolume() {
+  const [vol, setVol] = useState(0);
+  const [totalDays, setTotalDays] = useState(0);
 
   useEffect(() => {
-    let secTimer = setInterval(() => {
+    const intervalId = setInterval(() => {
+      var today = new Date();
+      var beginning = new Date(`1/1/${yyyy}`);
+      var difference = today.getTime() - beginning.getTime();
+      setVol(yyyy % 1000);
+      setTotalDays(Math.ceil(difference / (1000 * 60 * 60 * 24)));
+    }, 1000); // run every 24 hours
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: "Vol." + vol + " • No." + totalDays }}
+    />
+  );
+}
+
+export function NameOfDay() {
+  const [nameOfDay, setNameOfDay] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      var today = new Date();
+      var day = today.getDay();
+      setNameOfDay(days[day]);
+    }, 1000); // run every 24 hours
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <div dangerouslySetInnerHTML={{ __html: nameOfDay + "," }} />;
+}
+
+export function DateInformation() {
+  const [nameOfMonth, setNameOfMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      var today = new Date();
+      var mm = String(today.getMonth()).padStart(2, "0"); //January is 0!
+
+      setDay(String(today.getDate()).padStart(2, "0"));
+      setNameOfMonth(mL[parseInt(mm)]);
+      setYear(String(today.getFullYear()));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: nameOfMonth + "-" + day + "-" + year }}
+    />
+  );
+}
+
+export function CurrentTime() {
+  const [currentTime, setCurrentTime] = useState(today.toLocaleString());
+  useEffect(() => {
+    var secTimer = setInterval(() => {
       setCurrentTime(new Date().toLocaleString().split(",")[1].trim());
     }, 1000);
 
     return () => clearInterval(secTimer);
   }, []);
 
-  return <div>{"/" + currentTime}</div>;
+  return <div dangerouslySetInnerHTML={{ __html: "/ " + currentTime }} />;
 }
-
-export default CurrentTime;
--- app/Header.tsx
+++ app/Header.tsx
@@ -4,41 +4,14 @@ import NavLinks from "./NavLinks";
 import SearchBox from "./SearchBox";
 import Weather from "./Weather";
 import Typewriter from "./Typewriter/Typewriter";
-import CurrentTime from "./CurrentTime";
+import {
+  CurrentVolume,
+  NameOfDay,
+  DateInformation,
+  CurrentTime,
+} from "./CurrentTime";
 
 function Header() {
-  var today = new Date();
-  var day = today.getDay();
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  var mL = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  var dd = String(today.getDate()).padStart(2, "0");
-  var mm = String(today.getMonth()).padStart(2, "0"); //January is 0!
-  var yyyy = today.getFullYear();
-
-  let beginning = new Date(`1/1/${yyyy}`);
-  let difference = today.getTime() - beginning.getTime();
-  let totalDays = Math.ceil(difference / (1000 * 60 * 60 * 24));
   return (
     <div>
       <hr className="my-4 h-1 border-0 bg-black dark:bg-[#3A160E]" />
@@ -81,18 +54,21 @@ function Header() {
       <div className="py-auto mt-4 grid grid-cols-3 border-t-2 border-black dark:border-[#3A160E] dark:text-orange-500">
         <div className="pl-2 font-mono text-sm font-bold md:text-lg ">
           <div className="volslide pl-2">
-            Vol.{yyyy % 1000} • No.{totalDays}
+            <CurrentVolume />
           </div>
         </div>
         <div className="flex justify-center font-mono text-sm font-bold md:text-lg">
-          <div className="hidden sm:block">{days[day]}, </div>
+          <div className="hidden sm:block">
+            <NameOfDay />
+          </div>
           <div className="mx-1 ">
-            {mL[parseInt(mm)]}-{dd}-{yyyy}
+            <DateInformation />
           </div>
           <div className="hidden sm:block">
-            <CurrentTime today={today.toLocaleString()} />
+            <CurrentTime />
           </div>
         </div>
+
         <div className="flex items-center justify-end pr-2 font-mono text-sm font-bold md:text-lg">
           <Typewriter text={"Price:1¢"} />
         </div>
